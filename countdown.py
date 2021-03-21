c6eef38c6e6963c7714799a4f974a6710ceb7a80
Fabio F <48363702+fistadev@users.noreply.github.com>
2021-03-21 20:37:40 +0100

    Create countdown.py

---
+++ countdown.py
@@ -0,0 +1,10 @@
+for x in range(1, 11, 1):
+    print(x)
+print('Blast off!!')
+
+
+# I tried to create a countdown with the code below, but it's not working
+
+for x in range(1, 11, -1):
+    print(x)
+print('Blast off!!')
