dbaf6a6dd919af8c35829e81a36a6b5cf0d4218e
Fabio F <48363702+fistadev@users.noreply.github.com>
2021-03-21 20:45:28 +0100

    add while loop

--- countdown.py
+++ countdown.py
@@ -1,6 +1,10 @@
+# this is not a countdown, it's a normal counter
+
 for x in range(1, 11, 1):
     print(x)
 print('Blast off!!')
+
+
 
 
 # I tried to create a countdown with the code below, but it's not working
@@ -8,3 +12,15 @@ print('Blast off!!')
 for x in range(1, 11, -1):
     print(x)
 print('Blast off!!')
+
+
+
+
+# This countdown works
+
+i = 10
+while i >= 1:
+    print(i)
+    i -= 1
+
+print("Done with loop!")
